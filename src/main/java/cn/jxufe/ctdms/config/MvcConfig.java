2aff1f6e43949103993fa10e9d1d4b3729fd0517
叶志伟 <1059654342@qq.com>
2018-03-06 19:52:35 +0800

    init file

---
+++ src/main/java/cn/jxufe/ctdms/config/MvcConfig.java
@@ -0,0 +1,16 @@
+package cn.jxufe.ctdms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("login");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
