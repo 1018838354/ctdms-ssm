906c66691155f953c770944c1064bde2f07746e2
Caolijian <1018838354@qq.com>
2018-03-06 18:50:35 +0800

    Initial commit

---
+++ src/main/java/cn/jxufe/ctdms/CtdmsApplication.java
@@ -0,0 +1,12 @@
+package cn.jxufe.ctdms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtdmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtdmsApplication.class, args);
+	}
+}
